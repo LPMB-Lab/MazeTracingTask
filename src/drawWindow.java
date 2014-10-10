61afffaecaf588d2933352c2d466d35ff44a96ee
paytheo <omas.abdullah@gmail.com>
2014-10-10 15:19:08 -0400

    Add AntiAliasing
    * Added antialiasing for drawings

--- src/drawWindow.java
+++ src/drawWindow.java
@@ -72,6 +72,10 @@ public class drawWindow extends JPanel implements MouseListener
 	{
 		Graphics2D g2d = (Graphics2D) g;
 		
+		rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHints(rh);
+		
 		g2d.drawString("TEST",  50,  50);
 	}
 	@Override
